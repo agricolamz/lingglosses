a63ba382f530f08b2b1b1605b1c240f8a8800186
George Moroz <agricolamz@gmail.com>
2021-12-31 14:12:25 +0300

    add codecov

--- tests/testthat/test-add_gloss_gloss_example_make_gloss_list.R
+++ tests/testthat/test-add_gloss_gloss_example_make_gloss_list.R
@@ -4,6 +4,10 @@ test_that("add_gloss(), gloss_example(), make_gloss_list() test", {
     system.file("rmarkdown/templates/lingglosses-document/skeleton/",
                 package = "lingglosses", mustWork = TRUE), full.names = TRUE)
   file.copy(files, tmp)
-  rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE)
+  rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE,
+                    output_format = "html_document")
+  rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE,
+                    output_format = "pdf_document")
   expect_true(file.exists(paste0(tmp, "/skeleton.html")))
+  expect_true(file.exists(paste0(tmp, "/skeleton.pdf")))
 })
