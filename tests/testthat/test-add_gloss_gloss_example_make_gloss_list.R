d21215daae2d6a9a61cb602c4a77618ea664f9f1
George Moroz <agricolamz@gmail.com>
2021-12-31 14:29:21 +0300

    add suggestion

--- tests/testthat/test-add_gloss_gloss_example_make_gloss_list.R
+++ tests/testthat/test-add_gloss_gloss_example_make_gloss_list.R
@@ -6,8 +6,8 @@ test_that("add_gloss(), gloss_example(), make_gloss_list() test", {
   file.copy(files, tmp)
   rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE,
                     output_format = "html_document")
-  rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE,
-                    output_format = "pdf_document")
+  # rmarkdown::render(paste0(tmp, "/skeleton.Rmd"), quiet = TRUE,
+  #                   output_format = "pdf_document")
   expect_true(file.exists(paste0(tmp, "/skeleton.html")))
-  expect_true(file.exists(paste0(tmp, "/skeleton.pdf")))
+  # expect_true(file.exists(paste0(tmp, "/skeleton.pdf")))
 })
